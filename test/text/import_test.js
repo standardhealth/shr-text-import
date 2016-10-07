94f10e74b336049a17ee9406994f3c87becfd7cf
Chris Moesel <cmoesel@mitre.org>
2016-10-06 21:33:11 -0400

    Updated models and test fixtures
    Updated the models to reflect the changes to the SHR grammar.
    
    Updated the test fixtures to reflect the changes to the SHR grammar, and updated the tests to correctly reference the new fixtures.  Did NOT update test logic to use new models -- it still operates against old model definitions.  Did NOT update any translator logic either.

--- test/text/import_test.js
+++ test/text/import_test.js
@@ -1,20 +1,11 @@
 const {expect} = require('chai');
 const {importFromFilePath} = require('../../lib/text/import');
-const {Namespace, DataElement, Entry} = require('../../lib/models');
+const {Namespace, DataElement} = require('../../lib/models');
 
 describe('#importFromFilePath()', () => {
-  it('should correctly import a simple entry', () => {
-    let results = importFixture('simpleEntry');
-    let simple = expectAndGetSingleElement(results, 'shr.test', 'Simple', Entry);
-    expect(simple.description).to.equal('It is a simple entry');
-    expectSingleAnswer(simple, 'primitive', 'date');
-    expect(simple.valueset).to.be.undefined;
-    expect(simple.components).to.be.empty;
-  });
-
   it('should correctly import a simple data element', () => {
-    let results = importFixture('simpleDataElement');
-    let simple = expectAndGetSingleElement(results, 'shr.test', 'simple');
+    let results = importFixture('SimpleDataElement');
+    let simple = expectAndGetSingleElement(results, 'shr.test', 'Simple');
     expect(simple.description).to.equal('It is a simple data element');
     expectSingleAnswer(simple, 'primitive', 'string');
     expect(simple.valueset).to.be.undefined;
@@ -22,54 +13,54 @@ describe('#importFromFilePath()', () => {
   });
 
   it('should correctly import a coded data element', () => {
-    let results = importFixture('codedDataElement');
-    let coded = expectAndGetSingleElement(results, 'shr.test', 'coded');
+    let results = importFixture('CodedDataElement');
+    let coded = expectAndGetSingleElement(results, 'shr.test', 'Coded');
     expect(coded.description).to.equal('It is a coded data element');
     expectSingleAnswer(coded, 'primitive', 'code');
-    expect(coded.valueset).to.equal('http://standardhealthrecord.org/test/vs/coded');
+    expect(coded.valueset).to.equal('http://standardhealthrecord.org/test/vs/Coded');
     expect(coded.components).to.be.empty;
   });
 
   it('should correctly import a choice data element', () => {
-    let results = importFixture('choiceDataElement');
-    let simple = expectAndGetSingleElement(results, 'shr.test', 'choice');
+    let results = importFixture('ChoiceDataElement');
+    let simple = expectAndGetSingleElement(results, 'shr.test', 'Choice');
     expect(simple.description).to.equal('It is a data element with a choice');
     expect(simple.answers).to.have.length(3);
     expectAnswer(simple, 0, 'primitive', 'date');
-    expectAnswer(simple, 1, 'other.ns', 'period');
-    expectAnswer(simple, 2, 'shr.test', 'simple');
+    expectAnswer(simple, 1, 'other.ns', 'Period');
+    expectAnswer(simple, 2, 'shr.test', 'Simple');
     expect(simple.valueset).to.be.undefined;
     expect(simple.components).to.be.empty;
   });
 
-  it('should correctly import a composition', () => {
-    let results = importFixture('compositionDataElement');
-    let simple = expectAndGetSingleElement(results, 'shr.test', 'composition');
-    expect(simple.description).to.equal('It is a composition data element');
+  it('should correctly import a group', () => {
+    let results = importFixture('GroupOfThingsDataElement');
+    let simple = expectAndGetSingleElement(results, 'shr.test', 'GroupOfThings');
+    expect(simple.description).to.equal('It is a group data element');
     expect(simple.answers).to.be.empty;
     expect(simple.valueset).to.be.undefined;
     expect(simple.components).to.have.length(4);
-    expectComponent(simple, 0, 'shr.test', 'simple', 0, 1);
-    expectComponent(simple, 1, 'shr.test', 'coded', 0);
+    expectComponent(simple, 0, 'shr.test', 'Simple', 0, 1);
+    expectComponent(simple, 1, 'shr.test', 'Coded', 0);
     expectComponent(simple, 2, 'shr.test', 'Simple', 1);
-    expectComponent(simple, 3, 'other.ns', 'thing', 1, 1);
+    expectComponent(simple, 3, 'other.ns', 'Thing', 1, 1);
   });
 
   it('should correctly import multiple elements in a single namespace', () => {
-    let results = importFixture('multipleElements');
+    let results = importFixture('MultipleElements');
     expect(results).to.have.length(1);
     let ns = expectAndGetNamespace(results, 0, 'shr.test');
 
-    let simple = expectAndGetElement(ns, 0, 'Simple', Entry);
-    expect(simple.description).to.equal('It is a simple entry');
+    let simple = expectAndGetElement(ns, 0, 'SimpleDate');
+    expect(simple.description).to.equal('It is a simple date data element');
     expectSingleAnswer(simple, 'primitive', 'date');
     expect(simple.valueset).to.be.undefined;
     expect(simple.components).to.be.empty;
 
-    let coded = expectAndGetElement(ns, 1, 'coded');
+    let coded = expectAndGetElement(ns, 1, 'Coded');
     expect(coded.description).to.equal('It is a coded data element');
     expectSingleAnswer(coded, 'primitive', 'code');
-    expect(coded.valueset).to.equal('http://standardhealthrecord.org/test/vs/coded');
+    expect(coded.valueset).to.equal('http://standardhealthrecord.org/test/vs/Coded');
     expect(coded.components).to.be.empty;
   });
 });
