06c13830976ea33a5960b2e8c5750261f89d3b81
Chris Moesel <cmoesel@mitre.org>
2016-11-04 19:07:05 -0400

    Import sections, choices, refs, cardinalities
    Updated text importer to support importing sections, choices (or), and references.  Also support for cardinalities in values, choices, and parenthetical groupings.

--- test/text/import_test.js
+++ test/text/import_test.js
@@ -1,6 +1,6 @@
 const {expect} = require('chai');
 const {importFromFilePath} = require('../../lib/text/import');
-const {Namespace, DataElement, Group, Value, CodeValue, PrimitiveIdentifier} = require('../../lib/models');
+const {Namespace, Section, DataElement, Group, Value, CodeValue, RefValue, OrValues, QuantifiedValue, PrimitiveIdentifier} = require('../../lib/models');
 
 describe('#importFromFilePath()', () => {
   it('should correctly import a simple data element', () => {
@@ -17,17 +17,47 @@ describe('#importFromFilePath()', () => {
     expectCodeValue(coded.value, 'http://standardhealthrecord.org/test/vs/Coded');
   });
 
- /* it('should correctly import a choice data element', () => {
-    let results = importFixture('ChoiceDataElement');
-    let simple = expectAndGetSingleElement(results, 'shr.test', 'Choice');
-    expect(simple.description).to.equal('It is a data element with a choice');
-    expect(simple.answers).to.have.length(3);
-    expectAnswer(simple, 0, 'primitive', 'date');
-    expectAnswer(simple, 1, 'other.ns', 'Period');
-    expectAnswer(simple, 2, 'shr.test', 'Simple');
-    expect(simple.valueset).to.be.undefined;
-    expect(simple.components).to.be.empty;
-  });*/
+  it('should correctly import a reference to simple data element', () => {
+    const results = importFixture('simpleReferenceDataElement');
+    const simple = expectAndGetSingleElement(results, 'shr.test', 'SimpleReference');
+    expect(simple.description).to.equal('It is a reference to a simple data element');
+    expectRefValue(simple.value, 'shr.test', 'Simple');
+  });
+
+  it('should correctly import a data element with list value', () => {
+    const results = importFixture('multiStringDataElement');
+    const simple = expectAndGetSingleElement(results, 'shr.test', 'MultiString');
+    expect(simple.description).to.equal('It is a multi-string data element');
+    expectMinMax(simple.value, 1);
+    expectPrimitiveValue(simple.value.value, 'string');
+  });
+
+  it('should correctly import a choice data element', () => {
+    const results = importFixture('ChoiceDataElement');
+    const choice = expectAndGetSingleElement(results, 'shr.test', 'Choice');
+    expect(choice.description).to.equal('It is a data element with a choice');
+    expectOrValues(choice.value, 3);
+    expectOrValue(choice.value, 0, 'primitive', 'date');
+    expectOrValue(choice.value, 1, 'other.ns', 'Period');
+    expectOrValue(choice.value, 2, 'shr.test', 'Simple');
+  });
+
+  it('should correctly import a complex choice data element', () => {
+    const results = importFixture('ComplexChoiceDataElement');
+    const choice = expectAndGetSingleElement(results, 'shr.test', 'ComplexChoice');
+    expect(choice.description).to.equal('It is a data element with a complex choice');
+    expectMinMax(choice.value, 1, 2);
+    const or = choice.value.value;
+    expectOrValues(or, 2);
+    const or0 = or.values[0];
+    expectMinMax(or0, 3, 4);
+    expectOrValues(or0.value, 2);
+    expectOrValue(or0.value, 0, 'primitive', 'date');
+    expectOrValue(or0.value, 1, 'other.ns', 'Period');
+    const or1 = or.values[1];
+    expectMinMax(or1, 5, 6);
+    expectValue(or1.value, 'shr.test', 'Simple');
+  });
 
   it('should correctly import a group', () => {
     const results = importFixture('GroupOfThingsDataElement');
@@ -38,6 +68,20 @@ describe('#importFromFilePath()', () => {
     expectGroupElement(simple, 1, 'shr.test', 'Coded', 0);
     expectGroupElement(simple, 2, 'shr.test', 'Simple', 1);
     expectGroupElement(simple, 3, 'other.ns', 'Thing', 1, 1);
+  });
+
+  it('should correctly import a section', () => {
+    const results = importFixture('SimpleSection');
+    const namespace = expectAndGetNamespace(results, 0, 'shr.test');
+    expect(namespace.sections).to.have.length(1);
+    const section = namespace.sections[0];
+    expect(section).to.be.instanceof(Section);
+    expect(section.identifier.namespace).to.equal('shr.test');
+    expect(section.identifier.name).to.equal('SimpleSection');
+    expect(section.entries).to.have.length(3);
+    expectSectionEntry(section, 0, 'shr.test', 'Simple', 1);
+    expectSectionEntry(section, 1, 'shr.test', 'Coded', 1, 1);
+    expectSectionEntry(section, 2, 'shr.test', 'GroupOfThings', 0, 3);
   });
 
   it('should correctly import multiple elements in a single namespace', () => {
@@ -97,18 +141,48 @@ function expectCodeValue(value, expectedValueset) {
   expect(value.valueset).to.equal('http://standardhealthrecord.org/test/vs/Coded');
 }
 
-function expectGroupElement(group, componentIndex, expectedNamespace, expectedName, expectedMin, expectedMax) {
-  const element = group.elements[componentIndex];
+function expectRefValue(value, expectedNamespace, expectedName) {
+  expect(value).to.be.instanceof(RefValue);
+  expect(value.identifier.namespace).to.equal(expectedNamespace);
+  expect(value.identifier.name).to.equal(expectedName);
+}
 
-  expectValue(element.value, expectedNamespace, expectedName);
-  expect(element.min).to.equal(expectedMin);
-  if (typeof element.max != 'undefined') {
-    expect(element.max).to.equal(expectedMax);
-    expect(element.isMaxUnbounded()).to.be.false;
+function expectOrValues(value, size) {
+  expect(value).to.be.instanceof(OrValues);
+  expect(value.values).to.have.length(size);
+}
+
+function expectMinMax(quantifiedValue, expectedMin, expectedMax) {
+  expect(quantifiedValue).to.be.instanceof(QuantifiedValue);
+  expect(quantifiedValue.min).to.equal(expectedMin);
+  if (typeof quantifiedValue.max != 'undefined') {
+    expect(quantifiedValue.max).to.equal(expectedMax);
+    expect(quantifiedValue.isMaxUnbounded()).to.be.false;
   } else {
-    expect(element.max).to.be.undefined;
-    expect(element.isMaxUnbounded()).to.be.true;
+    expect(quantifiedValue.max).to.be.undefined;
+    expect(quantifiedValue.isMaxUnbounded()).to.be.true;
   }
+}
+
+function expectOrValue(or, componentIndex, expectedNamespace, expectedName, expectedMin, expectedMax) {
+  let value = or.values[componentIndex];
+  if (typeof expectedMin != 'undefined') {
+    expectMinMax(value, expectedMin, expectedMax);
+    value = value.value;
+  }
+  expectValue(value, expectedNamespace, expectedName);
+}
+
+function expectGroupElement(group, elementIndex, expectedNamespace, expectedName, expectedMin, expectedMax) {
+  const element = group.elements[elementIndex];
+  expectMinMax(element, expectedMin, expectedMax);
+  expectValue(element.value, expectedNamespace, expectedName);
+}
+
+function expectSectionEntry(section, sectionIndex, expectedNamespace, expectedName, expectedMin, expectedMax) {
+  const entry = section.entries[sectionIndex];
+  expectMinMax(entry, expectedMin, expectedMax);
+  expectValue(entry.value, expectedNamespace, expectedName);
 }
 
 function importFixture(name) {
