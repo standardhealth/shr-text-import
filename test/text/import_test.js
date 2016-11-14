aa4023b2b7dba8d3af56be071a9ba1c56b4050c6
Chris Moesel <cmoesel@mitre.org>
2016-11-14 17:51:51 -0500

    Change Text Importer Fixture Names
    Change names of fixtures used in text importer tests and add test to distinguish between entries and elements.

--- test/text/import_test.js
+++ test/text/import_test.js
@@ -3,51 +3,60 @@ const {importFromFilePath} = require('../../lib/text/import');
 const {Namespace, DataElement, Group, Value, CodeFromValueSetValue, RefValue, OrValues, QuantifiedValue, PrimitiveIdentifier} = require('../../lib/models');
 
 describe('#importFromFilePath()', () => {
-  it('should correctly import a simple data element', () => {
-    const results = importFixture('simpleDataElement');
-    const simple = expectAndGetSingleElement(results, 'shr.test', 'Simple');
+  it('should correctly import a simple entry', () => {
+    const results = importFixture('Simple');
+    const simple = expectAndGetSingleEntry(results, 'shr.test', 'Simple');
     expect(simple.concepts).to.have.length(1);
     expectConcept(simple.concepts[0], 'http://foo.org', 'bar');
-    expect(simple.description).to.equal('It is a simple data element');
+    expect(simple.description).to.equal('It is a simple entry');
     expectPrimitiveValue(simple.value, 'string');
   });
 
-  it('should correctly import a coded data element', () => {
-    const results = importFixture('CodedDataElement');
-    const coded = expectAndGetSingleElement(results, 'shr.test', 'Coded');
-    expect(coded.description).to.equal('It is a coded data element');
+  it('should correctly import a simple element', () => {
+    const results = importFixture('SimpleElement');
+    const simple = expectAndGetSingleElement(results, 'shr.test', 'Simple');
+    expect(simple.concepts).to.have.length(1);
+    expectConcept(simple.concepts[0], 'http://foo.org', 'bar');
+    expect(simple.description).to.equal('It is a simple element');
+    expectPrimitiveValue(simple.value, 'string');
+  });
+
+  it('should correctly import a coded entry', () => {
+    const results = importFixture('Coded');
+    const coded = expectAndGetSingleEntry(results, 'shr.test', 'Coded');
+    expect(coded.description).to.equal('It is a coded entry');
     expectCodeValue(coded.value, 'http://standardhealthrecord.org/test/vs/Coded');
   });
 
-  it('should correctly import a reference to simple data element', () => {
-    const results = importFixture('simpleReferenceDataElement');
-    const simple = expectAndGetSingleElement(results, 'shr.test', 'SimpleReference');
-    expect(simple.description).to.equal('It is a reference to a simple data element');
+  it('should correctly import a reference to simple element', () => {
+    const results = importFixture('SimpleReference');
+    const simple = expectAndGetSingleEntry(results, 'shr.test', 'SimpleReference');
+    expect(simple.description).to.equal('It is a reference to a simple element');
     expectRefValue(simple.value, 'shr.test', 'Simple');
   });
 
-  it('should correctly import a data element with list value', () => {
-    const results = importFixture('multiStringDataElement');
-    const simple = expectAndGetSingleElement(results, 'shr.test', 'MultiString');
-    expect(simple.description).to.equal('It is a multi-string data element');
+  it('should correctly import an entry with a list value', () => {
+    const results = importFixture('MultiString');
+    const simple = expectAndGetSingleEntry(results, 'shr.test', 'MultiString');
+    expect(simple.description).to.equal('It is a multi-string entry');
     expectMinMax(simple.value, 1);
     expectPrimitiveValue(simple.value.value, 'string');
   });
 
-  it('should correctly import a choice data element', () => {
-    const results = importFixture('ChoiceDataElement');
-    const choice = expectAndGetSingleElement(results, 'shr.test', 'Choice');
-    expect(choice.description).to.equal('It is a data element with a choice');
+  it('should correctly import a choice entry', () => {
+    const results = importFixture('Choice');
+    const choice = expectAndGetSingleEntry(results, 'shr.test', 'Choice');
+    expect(choice.description).to.equal('It is an entry with a choice');
     expectOrValues(choice.value, 3);
     expectOrValue(choice.value, 0, 'primitive', 'date');
     expectOrValue(choice.value, 1, 'other.ns', 'Period');
     expectOrValue(choice.value, 2, 'shr.test', 'Simple');
   });
 
-  it('should correctly import a complex choice data element', () => {
-    const results = importFixture('ComplexChoiceDataElement');
-    const choice = expectAndGetSingleElement(results, 'shr.test', 'ComplexChoice');
-    expect(choice.description).to.equal('It is a data element with a complex choice');
+  it('should correctly import a complex choice entry', () => {
+    const results = importFixture('ComplexChoice');
+    const choice = expectAndGetSingleEntry(results, 'shr.test', 'ComplexChoice');
+    expect(choice.description).to.equal('It is an entry with a complex choice');
     const or = choice.value;
     expectOrValues(or, 2);
     const or0 = or.values[0];
@@ -60,14 +69,29 @@ describe('#importFromFilePath()', () => {
     expectValue(or1.value, 'shr.test', 'Simple');
   });
 
-  it('should correctly import a group', () => {
-    const results = importFixture('GroupDataElement');
+  it('should correctly import a group entry', () => {
+    const results = importFixture('Group');
+    const simple = expectAndGetSingleEntry(results, 'shr.test', 'SimpleGroup', Group);
+    expect(simple.concepts).to.have.length(3);
+    expectConcept(simple.concepts[0], 'http://foo.org', 'bar');
+    expectConcept(simple.concepts[1], 'http://boo.org', 'far');
+    expectConcept(simple.concepts[2], 'ZOO', 'bear');
+    expect(simple.description).to.equal('It is a group entry');
+    expect(simple.elements).to.have.length(4);
+    expectGroupElement(simple, 0, 'shr.test', 'Simple', 0, 1);
+    expectGroupElement(simple, 1, 'shr.test', 'Coded', 0);
+    expectGroupElement(simple, 2, 'shr.test', 'Simple', 1);
+    expectGroupElement(simple, 3, 'other.ns', 'Thing', 1, 1);
+  });
+
+  it('should correctly import a group element', () => {
+    const results = importFixture('GroupElement');
     const simple = expectAndGetSingleElement(results, 'shr.test', 'SimpleGroup', Group);
     expect(simple.concepts).to.have.length(3);
     expectConcept(simple.concepts[0], 'http://foo.org', 'bar');
     expectConcept(simple.concepts[1], 'http://boo.org', 'far');
     expectConcept(simple.concepts[2], 'ZOO', 'bear');
-    expect(simple.description).to.equal('It is a group data element');
+    expect(simple.description).to.equal('It is a group element');
     expect(simple.elements).to.have.length(4);
     expectGroupElement(simple, 0, 'shr.test', 'Simple', 0, 1);
     expectGroupElement(simple, 1, 'shr.test', 'Coded', 0);
@@ -76,16 +100,16 @@ describe('#importFromFilePath()', () => {
   });
 
   it('should correctly import multiple elements in a single namespace', () => {
-    const results = importFixture('MultipleElements');
+    const results = importFixture('MultipleElementNamespace');
     expect(results).to.have.length(1);
     const ns = expectAndGetNamespace(results, 0, 'shr.test');
 
-    const simple = expectAndGetElement(ns, 0, 'SimpleDate');
-    expect(simple.description).to.equal('It is a simple date data element');
+    const simple = expectAndGetEntry(ns, 0, 'SimpleDate');
+    expect(simple.description).to.equal('It is a simple date entry');
     expectPrimitiveValue(simple.value, 'date');
 
     const coded = expectAndGetElement(ns, 1, 'Coded');
-    expect(coded.description).to.equal('It is a coded data element');
+    expect(coded.description).to.equal('It is a coded element');
     expectCodeValue(coded.value, 'http://standardhealthrecord.org/test/vs/Coded');
   });
 });
@@ -98,17 +122,34 @@ function expectAndGetNamespace(results, namespaceIndex, expectedNamespace) {
 }
 
 function expectAndGetElement(namespace, defIndex, expectedName, expectedClass=DataElement) {
+  return expectAndGetBaseElement(namespace, defIndex, expectedName, expectedClass, false);
+}
+
+function expectAndGetEntry(namespace, defIndex, expectedName, expectedClass=DataElement) {
+  return expectAndGetBaseElement(namespace, defIndex, expectedName, expectedClass, true);
+}
+
+function expectAndGetBaseElement(namespace, defIndex, expectedName, expectedClass, isEntry) {
   const def = namespace.definitions[defIndex];
   expect(def).to.be.instanceof(expectedClass);
+  expect(def.isEntry).to.equal(isEntry);
   expect(def.identifier.namespace).to.equal(namespace.namespace);
   expect(def.identifier.name).to.equal(expectedName);
   return def;
 }
 
 function expectAndGetSingleElement(results, expectedNamespace, expectedName, expectedClass=DataElement) {
+  return expectAndGetSingleBaseElement(results, expectedNamespace, expectedName, expectedClass, false);
+}
+
+function expectAndGetSingleEntry(results, expectedNamespace, expectedName, expectedClass=DataElement) {
+  return expectAndGetSingleBaseElement(results, expectedNamespace, expectedName, expectedClass, true);
+}
+
+function expectAndGetSingleBaseElement(results, expectedNamespace, expectedName, expectedClass, isEntry) {
   expect(results).to.have.length(1);
   const ns = expectAndGetNamespace(results, 0, expectedNamespace);
-  return expectAndGetElement(ns, 0, expectedName, expectedClass);
+  return expectAndGetBaseElement(ns, 0, expectedName, expectedClass, isEntry);
 }
 
 function expectValue(value, expectedNamespace, expectedName) {
